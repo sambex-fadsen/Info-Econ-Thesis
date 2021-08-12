5e7d900908b1624f3f91910e9dc7aab464d7b0fe
sambex-fadsen <sambexfadsen@gmail.com>
2021-08-12 14:38:33 -0600

    Edited code

--- Code/aumann.py
+++ Code/aumann.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 def partition(lst):
 	if len(lst)==1:
@@ -40,15 +41,15 @@ def communicate(world, event, agent_A, agent_B):
 	chunk_B=which_chunk(world,agent_B)
 	oldpssblchnks_A=range(len(agent_A))
 	oldpssblchnks_B=range(len(agent_B))
-	A_reports=[]
-	B_reports=[]
+	A_report=[]
+	B_report=[]
 
 	posterior_A=len(set(event) & (set(chunk_A) & set(unionchnks(agent_B, oldpssblchnks_B))))/len(set(chunk_A) & set(unionchnks(agent_B, oldpssblchnks_B)))
-	A_reports.append(posterior_A)
+	A_report.append(posterior_A)
 	newpssblchnks_A=pssblchnks(posterior_A, event, agent_A, agent_B, oldpssblchnks_B)
 
 	posterior_B=len(set(event) & (set(chunk_B) & set(unionchnks(agent_A, newpssblchnks_A))))/len(set(chunk_B) & set(unionchnks(agent_A, newpssblchnks_A)))
-	B_reports.append(posterior_B)
+	B_report.append(posterior_B)
 	newpssblchnks_B=pssblchnks(posterior_B, event, agent_B, agent_A, newpssblchnks_A)
 
 	while (newpssblchnks_A!=oldpssblchnks_A or newpssblchnks_B!=oldpssblchnks_B):
@@ -56,21 +57,45 @@ def communicate(world, event, agent_A, agent_B):
 		oldpssblchnks_B=newpssblchnks_B
 	
 		posterior_A=len(set(event) & (set(chunk_A) & set(unionchnks(agent_B, oldpssblchnks_B))))/len(set(chunk_A) & set(unionchnks(agent_B, oldpssblchnks_B)))
-		A_reports.append(posterior_A)
+		A_report.append(posterior_A)
 		newpssblchnks_A=pssblchnks(posterior_A, event, agent_A, agent_B, oldpssblchnks_B)
 
 		posterior_B=len(set(event) & (set(chunk_B) & set(unionchnks(agent_A, newpssblchnks_A))))/len(set(chunk_B) & set(unionchnks(agent_A, newpssblchnks_A)))
-		B_reports.append(posterior_B)
+		B_report.append(posterior_B)
 		newpssblchnks_B=pssblchnks(posterior_B, event, agent_B, agent_A, newpssblchnks_A)
-	return (A_reports,B_reports)
+
+	return (A_report,B_report)
+
+def check_rtrctv_rgrt(report):
+	Report=[1/2]+report
+	for i in range(1,len(Report)):
+		for j in range(i):
+			if Report[j]==0:
+				Report[j]=0.01
+			if Report[j+1]==0:
+				Report[j+1]=0.01
+			if Report[j]==1:
+				Report[j]=0.99
+			if Report[j+1]==1:
+				Report[j+1]=0.99
+			payoff=Report[i]*math.log(Report[j+1]/Report[j])+(1-Report[i])*math.log((1-Report[j+1])/(1-Report[j]))
+			if payoff<0:
+				return True
+	return False
+			
 
 world=1
-event=[1,5,9]
-agent_A=[[1,2,3],[4,5,6],[7,8,9]]
-agent_B=[[1,2,3,4],[5,6,7,8],[9]]
+event=[5,10,15,20]
 
-reports=communicate(world,event,agent_A,agent_B)
+rtrctv_rgrt=False
+while(rtrctv_rgrt==False):
+	agent_A=partition(list(range(1,26)))
+	agent_B=partition(list(range(1,26)))
+	reports=communicate(world,event,agent_A,agent_B)
+	if (check_rtrctv_rgrt(reports[0])==True or check_rtrctv_rgrt(reports[1])==True):
+		rtrctv_rgrt=True
 
 print(agent_A)
 print(agent_B)
 print(reports)
+print(num_it)
